fd1f66e1758a66cd5d154fd68f1bfbc2db5465e6
Ghorbani-Dev.ir <ghorbani.dev1985@gmail.com>
2023-11-29 12:56:29 +0330

    feat: develop AddNewBookList function

--- JS/Main.js
+++ JS/Main.js
@@ -11,3 +11,31 @@ let EditBookBtn = $.querySelector(".EditBookBtn");
 let BookListTable = $.querySelector(".BookListTable");
 let BookListItemsBody = $.querySelector(".BookListItemsBody");
 let NullBookList = $.querySelector(".NullBookList");
+let BookListItems = [];
+
+
+function AddNewBookList() {
+  let BookTitleInputValue = BookTitleInput.value.trim();
+  let BookSubjectInputValue = BookSubjectInput.value.trim();
+  let BookAuthorInputValue = BookAuthorInput.value.trim();
+  if (BookTitleInputValue || BookSubjectInputValue || BookAuthorInputValue) {
+    let NewBookListObj = {
+      id: BookListItems.length + 1,
+      BookTitle: BookTitleInputValue,
+      BookSubject: BookSubjectInputValue,
+      BookAuthor: BookAuthorInputValue,
+    };
+    GreenAlert();
+    BookListAlert.innerHTML = `کتاب با عنوان ${BookTitleInputValue} با موفقیت ثبت گردید.`;
+    RemoveAlert();
+    ClearInputs();
+    BookListItems.push(NewBookListObj);
+    SetLocalStorage(BookListItems);
+    BookListGenerator(BookListItems);
+    BookTitleInput.focus();
+  } else {
+    RedAlert();
+    BookListAlert.innerHTML = `لطفا همه موارد را وارد نمایید.`;
+    RemoveAlert();
+  }
+}
